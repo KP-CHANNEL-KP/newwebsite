12657b24ebe167ff09410d94d34129ca4f059a11
KP-CHANNEL-KP <kopaing232003@gmail.com>
2025-12-25 15:53:22 +0630

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
-console.log("Pro Business Website Loaded Successfully");
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 120,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.8,
+      random: true
+    },
+    size: {
+      value: 2,
+      random: true
+    },
+    move: {
+      enable: true,
+      speed: 0.6,
+      direction: "none",
+      out_mode: "out"
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    }
+  },
+  retina_detect: true
+});
